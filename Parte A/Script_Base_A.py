8acd8c80694328c7472b06fef6a128c1f3ecc34e
Yiyo <yiyo.erland@gmail.com>
2023-06-20 18:33:10 -0400

    scrip base de A

---
+++ Parte A/Script_Base_A.py
@@ -0,0 +1,31 @@
+import mariadb
+import sys
+
+# Connect to MariaDB Platform
+try:
+    conn = mariadb.connect(
+        user="prueba",
+        password="1234",
+        host="127.0.0.1",
+        port=3306
+    )
+
+except mariadb.Error as e:
+    print(f"Error connecting to MariaDB Platform: {e}")
+    sys.exit(1)
+
+# Get Cursor
+cur = conn.cursor()
+
+# Create Database
+query_create = "CREATE DATABASE Base_Datos_A"
+cur.execute(query_create)
+cur.execute("USE Base_Datos_A")
+cur.execute("CREATE TABLE Prensa (id CHAR(4), Region CHAR(16), Ciudad CHAR(32), Nombre CHAR(32), Contienen CHAR(16), pais CHAR(32), Año_Fundacion DATE, PRIMARY KEY(id))")
+cur.execute("CREATE TABLE Redes (Tipo_R CHAR(16), Usuario CHAR(32), IDPrensa CHAR(4),  PRIMARY KEY(Tipo_R,Usuario), FOREIGN KEY (IDPrensa) REFERENCES Prensa(id))")
+cur.execute("CREATE TABLE Cartegoria (Tipo CHAR(16), URL VARCHAR(2083), PRIMARY KEY(URL))")
+cur.execute("CREATE TABLE Fundadores (Fecha_N DATE, Nombre CHAR(32), ID CHAR(4), PRIMARY KEY(ID))")
+cur.execute("CREATE TABLE Ejemplo_N (XPATH_T CHAR(64), XPATH_C CHAR(128), XPATH_F CHAR(128), URL VARCHAR(2083), IDPrensa CHAR(4), PRIMARY KEY(Ubicacion)), FOREIGN KEY (IDPrensa) REFERENCES Prensa(id))")
+
+conn.commit() 
+conn.close()
