b42ff10085d8448fbe01c98799b1366b2e02bd05
Yiyo <yiyo.erland@gmail.com>
2023-06-28 11:43:50 -0400

    int

--- insert.py
+++ insert.py
@@ -23,6 +23,7 @@ def CrearCategoria():
     print("Categoria")
 
 def InsertarMedio():
+    insercion = True
     sigue = True
     #Tabla Prensa
     while(sigue):
@@ -42,30 +43,32 @@ def InsertarMedio():
     #consulta="INSERT INTO Prensa (id, Region, Ciudad, Nombre, Continente, pais, Año_Fundacion) VALUES (%s,%s,%s,%s,%s);"
     try:
         cur.execute("INSERT INTO Prensa (id, Region, Ciudad, Nombre, Continente, pais, Año_Fundacion) VALUES (?, ?, ?, ?, ?, ?, ?)",(IDMedio, infoPrensaArray[4], infoPrensaArray[5], infoPrensaArray[0], infoPrensaArray[2], infoPrensaArray[3], infoPrensaArray[1]))
+        print("Datos Insertados correctamente")
     except mariadb.Error as e: 
         print(f"Error: {e}")
 
     #Tabla Redes
-    """sigue=True
-    while(sigue):
-        infoPrensa=input("Por favor, ingrese el nombre del medio de prensa, su año de fundación, el continete de origen, el pais, la región y la ciudad. separe los datos con ', ': ")
-        infoPrensaArray=infoPrensa.split(", ")
-        while (len(infoPrensaArray)!= 6):
-            infoPrensa=input("Por favor, siga el formato establecido. ")
+    if(insercion):
+        """sigue=True
+        while(sigue):
+            infoPrensa=input(", ingrese el nombre del medio de prensa, su año de fundación, el continete de origen, el pais, la región y la ciudad. separe los datos con ', ': ")
             infoPrensaArray=infoPrensa.split(", ")
-        print(infoPrensaArray)
-        correcto=input("¿La información es correcta?, (Y/n): ")
-        if(correcto=="Y" or correcto=="y"):
-            sigue=False
-    ASCII="ABCDEFGHIJKLMNOPQRSTUVXYZ0123456789"
-    IDMedio=""
-    for i in range(0,4,1):
-        IDMedio+=ASCII[random.randint(0,len(ASCII)-1)]
-    #consulta="INSERT INTO Prensa (id, Region, Ciudad, Nombre, Continente, pais, Año_Fundacion) VALUES (%s,%s,%s,%s,%s);"
-    try:
-        cur.execute("INSERT INTO Prensa (id, Region, Ciudad, Nombre, Continente, pais, Año_Fundacion) VALUES (?, ?, ?, ?, ?, ?, ?)",(IDMedio, infoPrensaArray[4], infoPrensaArray[5], infoPrensaArray[0], infoPrensaArray[2], infoPrensaArray[3], infoPrensaArray[1]))
-    except mariadb.Error as e: 
-        print(f"Error: {e}")"""
+            while (len(infoPrensaArray)!= 6):
+                infoPrensa=input("Por favor, siga el formato establecido. ")
+                infoPrensaArray=infoPrensa.split(", ")
+            print(infoPrensaArray)
+            correcto=input("¿La información es correcta?, (Y/n): ")
+            if(correcto=="Y" or correcto=="y"):
+                sigue=False
+        ASCII="ABCDEFGHIJKLMNOPQRSTUVXYZ0123456789"
+        IDMedio=""
+        for i in range(0,4,1):
+            IDMedio+=ASCII[random.randint(0,len(ASCII)-1)]
+        #consulta="INSERT INTO Prensa (id, Region, Ciudad, Nombre, Continente, pais, Año_Fundacion) VALUES (%s,%s,%s,%s,%s);"
+        try:
+            cur.execute("INSERT INTO Prensa (id, Region, Ciudad, Nombre, Continente, pais, Año_Fundacion) VALUES (?, ?, ?, ?, ?, ?, ?)",(IDMedio, infoPrensaArray[4], infoPrensaArray[5], infoPrensaArray[0], infoPrensaArray[2], infoPrensaArray[3], infoPrensaArray[1]))
+        except mariadb.Error as e: 
+            print(f"Error: {e}")"""
     #Tabla Fundadores
 
     #Tabla Ejemplo_N
@@ -79,7 +82,7 @@ funcionando = True
 while(funcionando):
     VarConsulta = 0
     print("¿Qué desea insertar?")
-    print("1.- Medio de Prensa /n 2.- Categoría")
+    print("1.- Medio de Prensa / 2.- Categoría")
     while(VarConsulta != "1" and VarConsulta != "2"):
         VarConsulta=input()
         if (VarConsulta != "1" and VarConsulta != "2"):
